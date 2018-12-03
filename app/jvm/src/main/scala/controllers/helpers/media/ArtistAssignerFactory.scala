19f3f94ef0f50269fd45550ebdebbcf19229b995
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-03 08:43:48 +0100

    Implement ArtistAssignerFactory

--- app/jvm/src/main/scala/controllers/helpers/media/ArtistAssignerFactory.scala
+++ app/jvm/src/main/scala/controllers/helpers/media/ArtistAssignerFactory.scala
@@ -1,20 +1,56 @@
 package controllers.helpers.media
 
+import com.google.common.base.{CharMatcher, Splitter}
 import com.google.inject.Inject
+import common.CollectionUtils.getMostCommonStringIgnoringCase
+import common.GuavaReplacement.Iterables.getOnlyElement
 import controllers.helpers.media.ArtistAssignerFactory.ArtistAssigner
 import controllers.helpers.media.MediaScanner.MediaFile
 import models.access.JvmEntityAccess
+import models.media.Artist
+import models.slick.SlickUtils.dbRun
 
 final class ArtistAssignerFactory @Inject()(implicit entityAccess: JvmEntityAccess) {
 
   def fromMediaFiles(mediaFiles: Seq[MediaFile]): ArtistAssigner = {
-    // Also load all artists from entityAccess
-    ???
+    val storedLookupToCanonicalNameMap = {
+      val allArtists = dbRun(entityAccess.newSlickQuery[Artist]())
+      allArtists.map(_.name).groupBy(ArtistAssignerFactory.lookupName).mapValues(getOnlyElement)
+    }
+
+    val newLookupToCanonicalNameMap = {
+      val allArtists = mediaFiles.flatMap(media => media.artist ++ media.albumartist)
+      val newArtistNames = allArtists.filter(artist =>
+        !storedLookupToCanonicalNameMap.contains(ArtistAssignerFactory.lookupName(artist)))
+      newArtistNames.groupBy(ArtistAssignerFactory.lookupName).mapValues(getMostCommonStringIgnoringCase)
+    }
+
+    new ArtistAssigner(storedLookupToCanonicalNameMap ++ newLookupToCanonicalNameMap)
   }
 }
 
 object ArtistAssignerFactory {
-  final class ArtistAssigner {
-    def canonicalArtistName(artistName: String): String = ???
+
+  private val lookupNameCharMatcher: CharMatcher = {
+    val digits = CharMatcher.inRange('0', '9')
+    val letters = CharMatcher.inRange('a', 'z') or CharMatcher.inRange('A', 'Z')
+    digits or letters
+  }
+
+  private def lookupName(artistName: String): String = {
+    var interim = artistName.toLowerCase
+    for (split <- Seq("(", " f/", "/", "&", " and ", ", ")) {
+      val firstPart = Splitter.on(split).splitToList(interim).get(0)
+      if (firstPart.length >= 5) {
+        interim = firstPart
+      }
+    }
+    interim = interim.stripPrefix("the ")
+    lookupNameCharMatcher.retainFrom(interim)
+  }
+
+  final class ArtistAssigner(lookupToCanonicalNameMap: Map[String, String]) {
+
+    def canonicalArtistName(artistName: String): String = lookupToCanonicalNameMap(lookupName(artistName))
   }
 }
--- app/shared/src/main/scala/common/CollectionUtils.scala
+++ app/shared/src/main/scala/common/CollectionUtils.scala
@@ -8,4 +8,14 @@ object CollectionUtils {
   def toListMap[A, B](entries: Iterable[(A, B)]): ListMap[A, B] = ListMap(entries.toSeq: _*)
 
   def asMap[K, V](keys: Iterable[K], valueFunc: K => V): Map[K, V] = keys.map(k => k -> valueFunc(k)).toMap
+
+  def getMostCommonStringIgnoringCase(strings: Iterable[String]): String = {
+    require(strings.nonEmpty)
+    val mostCommonLowerCaseString = getMostCommonString(strings.map(_.toLowerCase))
+    getMostCommonString(strings.filter(_.toLowerCase == mostCommonLowerCaseString))
+  }
+
+  def getMostCommonString(strings: Iterable[String]): String = {
+    strings.groupBy(identity).mapValues(_.size).toSeq.minBy(-_._2)._1
+  }
 }
