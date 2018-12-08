235388e7dc458a8cfb81d6d740959ec2a3db9f72
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-08 11:39:31 +0100

    Expose media assets

--- app/jvm/src/main/scala/controllers/Application.scala
+++ app/jvm/src/main/scala/controllers/Application.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import java.nio.file.{Files, Paths}
+
+import akka.stream.scaladsl.StreamConverters
 import api.Picklers._
 import api.ScalaJsApiServerFactory
 import com.google.inject.Inject
@@ -8,6 +11,7 @@ import controllers.helpers.AuthenticatedAction
 import models.access.JvmEntityAccess
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import play.utils.UriEncoding
 
 final class Application @Inject()(implicit override val messagesApi: MessagesApi,
                                   components: ControllerComponents,
@@ -30,5 +34,35 @@ final class Application @Inject()(implicit override val messagesApi: MessagesApi
 
   def reactAppWithoutCredentials = Action { implicit request =>
     Ok(views.html.reactApp())
+  }
+
+  def mediaAssets(relativePath: String) = AuthenticatedAction { implicit user => implicit request =>
+    val mediaFolderPath =
+      Paths.get(
+        playConfiguration
+          .get[String]("app.media.mediaFolder")
+          .replaceFirst("^~", System.getProperty("user.home")))
+
+    val assetPath = mediaFolderPath resolve UriEncoding.decodePath(relativePath, "utf-8")
+
+    if (!Files.exists(assetPath)) {
+      NotFound(s"Could not find $assetPath")
+    } else if (Files.isDirectory(assetPath)) {
+      NotFound(s"Could not find $assetPath")
+    } else {
+      require(assetPath.toRealPath() startsWith mediaFolderPath, assetPath)
+
+      val connection = assetPath.toFile.toURI.toURL.openConnection()
+      val stream = connection.getInputStream
+      val source = StreamConverters.fromInputStream(() => stream)
+      RangeResult
+        .ofSource(
+          entityLength = stream.available(), // TODO: This may not be entirely accurate
+          source = source,
+          rangeHeader = request.headers.get(RANGE),
+          fileName = None,
+          contentType = None // TODO: Set content type
+        )
+    }
   }
 }
