0c657d6dfe50fa5b83571d7a39113120ce742474
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-25 23:32:29 +0100

    Canonicalise uielements.Module

--- app/js/shared/src/main/scala/hydro/flux/react/uielements/Module.scala
+++ app/js/shared/src/main/scala/hydro/flux/react/uielements/Module.scala
@@ -2,9 +2,12 @@ package hydro.flux.react.uielements
 
 import common.I18n
 import common.time.Clock
+import flux.react.uielements
 import flux.stores._
 import hydro.flux.action.Dispatcher
-import hydro.flux.stores.{ApplicationIsOnlineStore, PageLoadingStateStore, UserStore}
+import hydro.flux.stores.ApplicationIsOnlineStore
+import hydro.flux.stores.PageLoadingStateStore
+import hydro.flux.stores.UserStore
 import models.access.JsEntityAccess
 import models.user.User
 
@@ -19,9 +22,9 @@ final class Module(implicit i18n: I18n,
                    dispatcher: Dispatcher,
                    clock: Clock) {
 
+  lazy val pageHeader = new PageHeader
   lazy val globalMessages: GlobalMessages = new GlobalMessages
   lazy val pageLoadingSpinner: PageLoadingSpinner = new PageLoadingSpinner
   lazy val applicationDisconnectedIcon: ApplicationDisconnectedIcon = new ApplicationDisconnectedIcon
   lazy val pendingModificationsCounter: PendingModificationsCounter = new PendingModificationsCounter
-  lazy val pageHeader = new PageHeader
 }
