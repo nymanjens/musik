8307d2db17479aa4b345eb4f2d0d3664d5bde01d
Jens Nyman <nymanjens.nj@gmail.com>
2018-12-29 20:49:01 +0100

    Create SecondaryIndexFunction

--- app/js/shared/src/main/scala/app/models/access/Module.scala
+++ app/js/shared/src/main/scala/app/models/access/Module.scala
@@ -2,12 +2,22 @@ package app.models.access
 
 import app.api.ScalaJsApi.GetInitialDataResponse
 import app.api.ScalaJsApiClient
+import app.models.access.LocalDatabaseImpl.SecondaryIndexFunction
+import app.models.modification.EntityType.AlbumType
+import app.models.modification.EntityType.ArtistType
+import app.models.modification.EntityType.PlayStatusType
+import app.models.modification.EntityType.PlaylistEntryType
+import app.models.modification.EntityType.SongType
+import app.models.modification.EntityType.UserType
 import app.models.user.User
+
+import scala.collection.immutable.Seq
 
 final class Module(implicit user: User,
                    scalaJsApiClient: ScalaJsApiClient,
                    getInitialDataResponse: GetInitialDataResponse) {
 
+  implicit val secondaryIndexFunction = Module.secondaryIndexFunction
   implicit val entityModificationPushClientFactory: EntityModificationPushClientFactory =
     new EntityModificationPushClientFactory()
 
@@ -22,4 +32,14 @@ final class Module(implicit user: User,
 
     entityAccess
   }
+}
+object Module {
+  val secondaryIndexFunction: SecondaryIndexFunction = SecondaryIndexFunction({
+    case UserType          => Seq()
+    case SongType          => Seq(ModelField.Song.albumId, ModelField.Album.artistId)
+    case AlbumType         => Seq(ModelField.Album.artistId)
+    case ArtistType        => Seq()
+    case PlaylistEntryType => Seq()
+    case PlayStatusType    => Seq()
+  })
 }
