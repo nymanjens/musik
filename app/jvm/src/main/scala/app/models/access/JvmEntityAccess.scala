aeb38083a2c3e2a8476b9af7070488f914152ab0
Jens Nyman <nymanjens.nj@gmail.com>
2019-01-03 15:58:27 +0100

    hydro-mv app.models.slick.SlickEntityManager hydro.models.slick.SlickEntityManager

--- app/jvm/src/main/scala/app/models/access/JvmEntityAccess.scala
+++ app/jvm/src/main/scala/app/models/access/JvmEntityAccess.scala
@@ -15,7 +15,7 @@ import hydro.models.modification.EntityModification
 import app.models.modification.EntityModificationEntity
 import hydro.models.modification.EntityType
 import app.models.modification.EntityTypes
-import app.models.slick.SlickEntityManager
+import hydro.models.slick.SlickEntityManager
 import app.models.slick.SlickEntityTableDef
 import app.models.slick.SlickUtils.dbApi._
 import app.models.slick.SlickUtils.dbRun
