254e813407e088044222b3b9093d0e7366aa8f99
Jens Nyman <nymanjens.nj@gmail.com>
2019-02-09 15:50:46 +0100

    InMemoryEntityDatabaseTest: Make sorting/filter/limit tests more elaborate

--- app/jvm/src/test/scala/hydro/models/access/InMemoryEntityDatabaseTest.scala
+++ app/jvm/src/test/scala/hydro/models/access/InMemoryEntityDatabaseTest.scala
@@ -9,7 +9,9 @@ import hydro.models.modification.EntityModification
 import hydro.models.modification.EntityType
 import app.models.user.User
 import hydro.models.Entity
+import hydro.models.access.DbQuery.Sorting
 import hydro.models.access.DbQueryImplicits._
+import hydro.models.access.InMemoryEntityDatabase.Sortings
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 
@@ -24,70 +26,89 @@ class InMemoryEntityDatabaseTest extends HookedSpecification {
   val user1 = createUser(loginName = "login1", name = "name3")
   val user2 = createUser(loginName = "login2", name = "name2")
   val user3 = createUser(loginName = "login3", name = "name1")
+  val user4 = createUser(loginName = "login4", name = "name0")
 
   "queryExecutor()" in {
     entitiesFetcher.users ++= Seq(user1, user2, user3)
 
-    val database = new InMemoryEntityDatabase(entitiesFetcher)
+    implicit val database = new InMemoryEntityDatabase(
+      entitiesFetcher,
+      sortings = Sortings.create.withSorting(Sorting.ascBy(ModelFields.User.loginName)))
 
-    runTestsAssumingUser123(database)
+    "all data" in {
+      newUserQuery().data() must containTheSameElementsAs(Seq(user1, user2, user3))
+    }
+    "filter" in {
+      newUserQuery().filter(ModelFields.User.name === "name2").data() mustEqual Seq(user2)
+    }
+    "limit" in {
+      newUserQuery().sort(Sorting.ascBy(ModelFields.User.loginName)).limit(2).data() mustEqual
+        Seq(user1, user2)
+    }
+    "sorting" in {
+      assertDatabaseContainsExactlySorted(user1, user2, user3)
+    }
   }
 
   "update()" in {
     "Add" in {
       entitiesFetcher.users ++= Seq(user1, user2)
-      val database = new InMemoryEntityDatabase(entitiesFetcher)
-      DbResultSet
-        .fromExecutor(database.queryExecutor[User])
-        .data() // ensure lazy fetching gets triggered (if any)
+      implicit val database = new InMemoryEntityDatabase(entitiesFetcher)
+      triggerLazyFetching(database)
 
       entitiesFetcher.users += user3
       database.update(EntityModification.Add(user3))
 
-      runTestsAssumingUser123(database)
+      assertDatabaseContainsExactlySorted(user1, user2, user3)
     }
 
     "Remove" in {
-      val user4 = createUser(loginName = "login4", name = "name0")
-
       entitiesFetcher.users ++= Seq(user1, user2, user3, user4)
-      val database = new InMemoryEntityDatabase(entitiesFetcher)
-      DbResultSet
-        .fromExecutor(database.queryExecutor[User])
-        .data() // ensure lazy fetching gets triggered (if any)
+      implicit val database = new InMemoryEntityDatabase(entitiesFetcher)
+      triggerLazyFetching(database)
 
       entitiesFetcher.users -= user4
       database.update(EntityModification.createRemove(user4))
 
-      runTestsAssumingUser123(database)
+      assertDatabaseContainsExactlySorted(user1, user2, user3)
     }
 
     "Update" in {
       val user2AtCreate = createUser(id = user2.id, loginName = "login9", name = "name99")
 
       entitiesFetcher.users ++= Seq(user1, user2AtCreate, user3)
-      val database = new InMemoryEntityDatabase(entitiesFetcher)
-      DbResultSet
-        .fromExecutor(database.queryExecutor[User])
-        .data() // ensure lazy fetching gets triggered (if any)
+      implicit val database = new InMemoryEntityDatabase(entitiesFetcher)
+      triggerLazyFetching(database)
 
       entitiesFetcher.users -= user2AtCreate
       entitiesFetcher.users += user2
       database.update(EntityModification.Update(user2))
 
-      runTestsAssumingUser123(database)
+      assertDatabaseContainsExactlySorted(user1, user2, user3)
     }
   }
 
-  private def runTestsAssumingUser123(database: InMemoryEntityDatabase) = {
-    val executor = database.queryExecutor[User]
-
-    "filtered" in {
-      DbResultSet
-        .fromExecutor(executor)
-        .filter(ModelFields.User.name === "name2")
-        .data() mustEqual Seq(user2)
+  private def assertDatabaseContainsExactlySorted(users: User*)(implicit database: InMemoryEntityDatabase) = {
+    "assertDatabaseContainsExactlySorted" in {
+      "cached asc sorting" in {
+        newUserQuery().sort(Sorting.ascBy(ModelFields.User.loginName)).data() mustEqual users
+      }
+      "cached desc sorting" in {
+        newUserQuery().sort(Sorting.descBy(ModelFields.User.loginName)).data() mustEqual users.reverse
+      }
+      "non-cached sorting" in {
+        newUserQuery().sort(Sorting.ascBy(ModelFields.User.name)).data() mustEqual users.reverse
+      }
     }
+  }
+
+  private def newUserQuery()(implicit database: InMemoryEntityDatabase): DbResultSet.Sync[User] =
+    DbResultSet.fromExecutor(database.queryExecutor[User])
+
+  private def triggerLazyFetching(database: InMemoryEntityDatabase): Unit = {
+    DbResultSet
+      .fromExecutor(database.queryExecutor[User])
+      .data() // ensure lazy fetching gets triggered (if any)
   }
 
   private def createUser(id: Long = -1, loginName: String, name: String): User = {
