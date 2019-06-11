30f4dd54ce4c13059d8dd234ae81fcfaeec313b7
Jens Nyman <nymanjens.nj@gmail.com>
2019-06-11 23:02:42 +0100

    [Bump version to v0.02.x03]

--- app/shared/src/main/scala/app/AppVersion.scala
+++ app/shared/src/main/scala/app/AppVersion.scala
@@ -2,5 +2,5 @@ package app
 
 /** Version string that gets incremented on every deploy. */
 object AppVersion {
-  val versionString: String = "0.02.x02"
+  val versionString: String = "0.02.x03"
 }
