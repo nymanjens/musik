03390c31b9009195ba609dd069f045fd411ee8fe
Jens Nyman <nymanjens.nj@gmail.com>
2019-06-18 09:44:51 +0200

    Finish MusikEntitySyncLogicTest

--- app/js/client/src/test/scala/app/models/access/MusikEntitySyncLogicTest.scala
+++ app/js/client/src/test/scala/app/models/access/MusikEntitySyncLogicTest.scala
@@ -1,27 +1,29 @@
 package app.models.access
 
-import scala.collection.immutable.Seq
-import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
-import scala.async.Async.async
-import scala.async.Async.await
-import app.common.testing.TestObjects._
 import app.common.testing.FakeScalaJsApiClient
 import app.common.testing.TestObjects
-import app.models.media.PlaylistEntry
-import app.models.media.Song
+import app.common.testing.TestObjects._
 import app.models.media.Album
 import app.models.media.Artist
+import app.models.media.PlaylistEntry
+import app.models.media.PlayStatus
+import app.models.media.Song
 import hydro.common.testing.Awaiter
 import hydro.common.testing.FakeLocalDatabase
-import hydro.common.testing.FakeLocalDatabase
+import hydro.models.access.DbQueryImplicits._
 import hydro.models.access.DbResultSet
 import hydro.models.modification.EntityType
 import hydro.models.Entity
+import hydro.models.access.DbQuery
 import hydro.models.modification.EntityModification
 import utest._
 
+import scala.async.Async.async
+import scala.async.Async.await
+import scala.collection.immutable.Seq
 import scala.concurrent.Future
 import scala.language.reflectiveCalls
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
 object MusikEntitySyncLogicTest extends TestSuite {
 
@@ -53,11 +55,30 @@ object MusikEntitySyncLogicTest extends TestSuite {
     }
 
     "canBeExecutedLocally" - {
-      "yes" - {
-        1 ==> 1
+      "PlayStatus query" - async {
+        await(
+          logic.canBeExecutedLocally(
+            DbQuery[PlayStatus](
+              filter = ModelFields.PlayStatus.userId isAnyOf Seq(1, 2, 3),
+              sorting = None,
+              limit = None),
+            db)) ==> true
       }
-      "no" - {
-        1 ==> 1
+      "Song lookup (local)" - {
+        db.addAll(Seq(testSong))
+
+        "Song is persisted locally" - async {
+          await(
+            logic.canBeExecutedLocally(
+              DbQuery[Song](filter = ModelFields.Song.id === testSong.id, sorting = None, limit = None),
+              db)) ==> true
+        }
+        "Song is missing locally" - async {
+          await(
+            logic.canBeExecutedLocally(
+              DbQuery[Song](filter = ModelFields.Song.id === 9999, sorting = None, limit = None),
+              db)) ==> false
+        }
       }
     }
 
