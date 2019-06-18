d12e16121d44c29e0d1adb051fcce69e4655df31
Jens Nyman <nymanjens.nj@gmail.com>
2019-06-18 22:44:55 +0100

    PlayStatusStore now combines Local storage and an entity store

--- app/js/shared/src/main/scala/app/flux/stores/media/PlayStatusStore.scala
+++ app/js/shared/src/main/scala/app/flux/stores/media/PlayStatusStore.scala
@@ -1,17 +1,19 @@
 package app.flux.stores.media
 
-import app.flux.stores.media.PlayStatusStore.State
 import app.models.access.ModelFields
 import app.models.media.JsPlaylistEntry
-import app.models.media.PlayStatus
 import app.models.media.PlaylistEntry
-import hydro.models.modification.EntityModification
+import app.models.media.PlayStatus
 import app.models.user.User
 import hydro.common.time.Clock
 import hydro.flux.action.Dispatcher
 import hydro.flux.stores.AsyncEntityDerivedStateStore
+import hydro.flux.stores.CombiningStateStore
+import hydro.flux.stores.StateStore
 import hydro.models.access.JsEntityAccess
 import hydro.models.access.ModelField
+import hydro.models.modification.EntityModification
+import org.scalajs.dom
 
 import scala.async.Async.async
 import scala.async.Async.await
@@ -19,11 +21,16 @@ import scala.collection.immutable.Seq
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
-final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
-                            user: User,
-                            dispatcher: Dispatcher,
-                            clock: Clock)
-    extends AsyncEntityDerivedStateStore[State] {
+final class PlayStatusStore private (entityDerivedStore: PlayStatusStore.EntityDerived,
+                                     fromLocalStorageStore: PlayStatusStore.FromLocalStorage)(
+    implicit entityAccess: JsEntityAccess,
+    user: User,
+    dispatcher: Dispatcher,
+    clock: Clock)
+    extends CombiningStateStore[
+      Option[PlayStatusStore.EntityDerived.State],
+      PlayStatusStore.FromLocalStorage.State,
+      Option[PlayStatusStore.State]](entityDerivedStore, fromLocalStorageStore) {
 
   private var playWasEverStartedInThisSession: Boolean = false
 
@@ -35,8 +42,9 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
   def togglePlay(playing: java.lang.Boolean = null): Future[Unit] = async {
     val newHasStarted: Boolean = playing match {
       case null =>
-        val state = await(stateFuture)
-        !state.hasStarted
+        val combinedState =
+          combineStoreStates(await(entityDerivedStore.stateFuture), fromLocalStorageStore.state)
+        !combinedState.hasStarted
       case bool => bool
     }
     if (newHasStarted) {
@@ -46,7 +54,8 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
   }
 
   def toggleStopAfterCurrentSong(): Future[Unit] = async {
-    await(upsertPlayStatus(stopAfterCurrentSong = !await(stateFuture).stopAfterCurrentSong))
+    await(
+      upsertPlayStatus(stopAfterCurrentSong = !await(entityDerivedStore.stateFuture).stopAfterCurrentSong))
   }
 
   /** Returns true if anything changed */
@@ -67,7 +76,7 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
   }
 
   def indicateSongEnded(): Future[Unit] = async {
-    if (await(stateFuture).stopAfterCurrentSong) {
+    if (await(entityDerivedStore.stateFuture).stopAfterCurrentSong) {
       await(upsertPlayStatus(hasStarted = false, stopAfterCurrentSong = false))
     }
     val advanced = await(advanceEntriesInPlaylist(step = +1))
@@ -76,6 +85,10 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
       // This is the last song of the playlist
       await(upsertPlayStatus(hasStarted = false))
     }
+  }
+
+  def toggleRemoteControl(): Unit = {
+    fromLocalStorageStore.setIsRemoteControl(fromLocalStorageStore.state.isRemoteControl)
   }
 
   private def upsertPlayStatus(currentPlaylistEntryId: java.lang.Long = null,
@@ -114,7 +127,7 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
       case None =>
         val maybeEntryId: Option[Long] =
           if (currentPlaylistEntryId != null) Some(currentPlaylistEntryId)
-          else await(stateFuture).currentPlaylistEntry.map(_.id)
+          else await(entityDerivedStore.stateFuture).currentPlaylistEntry.map(_.id)
         maybeEntryId match {
           case Some(entryId) =>
             Seq(
@@ -132,34 +145,116 @@ final class PlayStatusStore(implicit entityAccess: JsEntityAccess,
   }
 
   // **************** Implementation of base class methods **************** //
-  override protected def calculateState(): Future[State] = async {
-    val maybePlayStatus = await(PlayStatus.get())
-    val currentPlaylistEntryId = maybePlayStatus.map(_.currentPlaylistEntryId) match {
-      case Some(id) => Some(id)
-      case None     => await(PlaylistEntry.getOrderedSeq()).headOption.map(_.id)
+  override protected def combineStoreStates(
+      maybeEntityDerivedState: Option[PlayStatusStore.EntityDerived.State],
+      localStorageState: PlayStatusStore.FromLocalStorage.State): Option[PlayStatusStore.State] = {
+    for (entityDerivedState <- maybeEntityDerivedState)
+      yield combineStoreStates(entityDerivedState, localStorageState)
+  }
+
+  private def combineStoreStates(
+      entityDerivedState: PlayStatusStore.EntityDerived.State,
+      localStorageState: PlayStatusStore.FromLocalStorage.State): PlayStatusStore.State = {
+    val isRemoteControl = localStorageState.isRemoteControl
+
+    PlayStatusStore.State(
+      currentPlaylistEntry = entityDerivedState.currentPlaylistEntry,
+      hasStarted = entityDerivedState.hasStarted && (playWasEverStartedInThisSession || isRemoteControl),
+      stopAfterCurrentSong = entityDerivedState.stopAfterCurrentSong,
+      isRemoteControl = isRemoteControl,
+    )
+  }
+}
+object PlayStatusStore {
+
+  def apply()(implicit entityAccess: JsEntityAccess,
+              user: User,
+              dispatcher: Dispatcher,
+              clock: Clock): PlayStatusStore = {
+    new PlayStatusStore(new EntityDerived(), new FromLocalStorage)
+  }
+
+  case class State(currentPlaylistEntry: Option[JsPlaylistEntry],
+                   hasStarted: Boolean,
+                   stopAfterCurrentSong: Boolean,
+                   isRemoteControl: Boolean,
+  )
+  object State {
+    def nullInstance: State =
+      State(
+        currentPlaylistEntry = None,
+        hasStarted = false,
+        stopAfterCurrentSong = false,
+        isRemoteControl = false,
+      )
+  }
+
+  private final class EntityDerived(implicit entityAccess: JsEntityAccess,
+                                    user: User,
+                                    dispatcher: Dispatcher,
+                                    clock: Clock)
+      extends AsyncEntityDerivedStateStore[EntityDerived.State] {
+
+    // **************** Implementation of base class methods **************** //
+    override protected def calculateState(): Future[EntityDerived.State] = async {
+      val maybePlayStatus = await(PlayStatus.get())
+      val currentPlaylistEntryId = maybePlayStatus.map(_.currentPlaylistEntryId) match {
+        case Some(id) => Some(id)
+        case None     => await(PlaylistEntry.getOrderedSeq()).headOption.map(_.id)
+      }
+      val currentPlaylistEntry =
+        if (currentPlaylistEntryId.isDefined)
+          Some(await(JsPlaylistEntry.fromEntityId(currentPlaylistEntryId.get)))
+        else None
+      EntityDerived.State(
+        currentPlaylistEntry = currentPlaylistEntry,
+        hasStarted = maybePlayStatus.map(_.hasStarted) getOrElse false,
+        stopAfterCurrentSong = maybePlayStatus.map(_.stopAfterCurrentSong) getOrElse false
+      )
     }
-    val currentPlaylistEntry =
-      if (currentPlaylistEntryId.isDefined)
-        Some(await(JsPlaylistEntry.fromEntityId(currentPlaylistEntryId.get)))
-      else None
-    State(
-      currentPlaylistEntry = currentPlaylistEntry,
-      hasStarted = maybePlayStatus.map(_.hasStarted && playWasEverStartedInThisSession) getOrElse false,
-      stopAfterCurrentSong = maybePlayStatus.map(_.stopAfterCurrentSong) getOrElse false
+
+    override protected def modificationImpactsState(entityModification: EntityModification,
+                                                    state: EntityDerived.State): Boolean =
+      entityModification.entityType == PlayStatus.Type ||
+        entityModification.entityType == PlaylistEntry.Type
+
+  }
+  object EntityDerived {
+    case class State(currentPlaylistEntry: Option[JsPlaylistEntry],
+                     hasStarted: Boolean,
+                     stopAfterCurrentSong: Boolean,
     )
   }
 
-  override protected def modificationImpactsState(entityModification: EntityModification,
-                                                  state: State): Boolean =
-    entityModification.entityType == PlayStatus.Type ||
-      entityModification.entityType == PlaylistEntry.Type
-}
-object PlayStatusStore {
-  case class State(currentPlaylistEntry: Option[JsPlaylistEntry],
-                   hasStarted: Boolean,
-                   stopAfterCurrentSong: Boolean)
-  object State {
-    def nullInstance: State =
-      State(currentPlaylistEntry = None, hasStarted = false, stopAfterCurrentSong = false)
+  private final class FromLocalStorage extends StateStore[FromLocalStorage.State] {
+
+    private val remoteControlStorageKey: String = "remote-control"
+    private var remoteControlValueCache: Option[Boolean] = None
+
+    // **************** API **************** //
+    def setIsRemoteControl(value: Boolean): Unit = {
+      val previousValue = state.isRemoteControl
+      dom.window.localStorage.setItem(remoteControlStorageKey, value.toString)
+      remoteControlValueCache = Some(value)
+      if (previousValue != value) {
+        invokeStateUpdateListeners()
+      }
+    }
+
+    // **************** Implementation of base class methods **************** //
+    override def state: FromLocalStorage.State = {
+      val isRemoteControl = remoteControlValueCache getOrElse {
+        dom.window.localStorage.getItem(remoteControlStorageKey) match {
+          case "true"  => true
+          case "false" => false
+          case null    => false
+        }
+      }
+      FromLocalStorage.State(isRemoteControl = isRemoteControl)
+    }
   }
+  object FromLocalStorage {
+    case class State(isRemoteControl: Boolean)
+  }
+
 }
